9d091d37134d5739313c2fe18d3dd6c4577fdd71
Herman Singh <kartstig@gmail.com>
2019-02-03 22:30:34 -0500

    Add some randomish speaker generation

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -15,12 +15,41 @@ alias CbusElixir.App.Meeting
 
 start_date = ~N[2018-03-01T00:00:00]
 
+speakers = [
+  %{
+    name: "Uncle Bob",
+    url: "https://blog.cleancoder.com/",
+    email: "test@example.com"
+  },
+  %{
+    name: "Gary Berhhardt",
+    url: "https://twitter.com/garybernhardt",
+    email: "test@example.com"
+
+  },
+  %{
+    name: "Joe Armstrong",
+    url: "https://twitter.com/joeer",
+    email: "test@example.com"
+  }
+]
+
 Enum.each(0..100, fn x ->
   date = Timex.shift(start_date, months: x)
   days = Enum.find(0..6, fn d -> Timex.shift(date, days: d) |> Date.day_of_week() == 2 end)
   meeting_date = Timex.shift(date, days: days)
 
-  %Meeting{}
+  m = %Meeting{}
   |> Meeting.changeset(%{date: meeting_date})
   |> Repo.insert!()
+
+  Enum.each(1..3, fn s ->
+    %Speaker{}
+    |> Map.merge(Enum.random(speakers))
+    |> Speaker.changeset(%{
+      meeting_id: m.id,
+      title: :crypto.strong_rand_bytes(30) |> Base.url_encode64()
+    })
+    |> Repo.insert!()
+  end)
 end)
